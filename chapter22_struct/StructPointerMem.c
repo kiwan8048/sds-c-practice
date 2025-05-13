c23fc884c6e667f8776f15f555e43167076d06f0
kiwan8048 <koh0916b@gmail.com>
2025-05-13 17:43:59 +0900

    Update StructPointerMem.c

--- chapter22_struct/StructPointerMem.c
+++ chapter22_struct/StructPointerMem.c
@@ -1,3 +1,4 @@
+// 이 코드는 교재의 내용을 참고하여 작성하였습니다
 #include <stdio.h>
 
 struct point
@@ -17,7 +18,7 @@ int main(void)
 	struct point cen = {2, 7};
 	double rad = 5.5;
 	struct circle ring = { rad, &cen };
-	printf("���� ������: %g \n", ring.radius);
-	printf("���� �߽� [%d, %d] \n", (ring.center)->xpos, (ring.center)->ypos);
+	printf("¿øÀÇ ¹ÝÁö¸§: %g \n", ring.radius);
+	printf("¿øÀÇ Áß½É [%d, %d] \n", (ring.center)->xpos, (ring.center)->ypos);
 	return 0;
 }
