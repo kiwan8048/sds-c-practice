55db8ee381dc8dc4e32e4aaee9304969c449bbab
kiwan8048 <koh0916b@gmail.com>
2025-05-16 02:08:19 +0900

    Add files via upload

---
+++ chapter23_typedef/StructFunctionCallByRef.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct point
+{
+	int xpos;
+	int ypos;
+} Point;
+
+void OrgSymTrans(Point* ptr)
+{
+	ptr->xpos = (ptr->xpos) * -1; // 부호 뒤집기
+	ptr->ypos = (ptr->ypos) * -1;
+}
+
+void ShowPosition(Point pos)
+{
+	printf("[%d, %d] \n", pos.xpos, pos.ypos);
+}
+
+int main(void)
+{
+	Point pos = { 7,-5 };
+	OrgSymTrans(&pos); // 원점 대칭
+	ShowPosition(pos);
+	OrgSymTrans(&pos); // 원상 복귀
+	ShowPosition(pos);
+	return 9;
+}
