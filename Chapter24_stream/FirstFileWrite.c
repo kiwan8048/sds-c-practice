d5e4389200845f48a66c54cf51d99ed0ec22a1b4
kiwan8048 <koh0916b@gmail.com>
2025-05-19 10:07:07 +0900

    Update FirstFileWrite.c

--- Chapter24_stream/FirstFileWrite.c
+++ Chapter24_stream/FirstFileWrite.c
@@ -1,1 +1,17 @@
+// 이 코드는 교재의 내용을 참고하였습니다
+#include <stdio.h>
 
+int main(void)
+{
+	FILE* fp = fopen("data.txt", "wt"); // 쓰기 스트림으로 FILE 구조체 형성하고 그 주소를 fp에 저장
+	if (fp == NULL)
+	{
+		puts("파일 오픈 실패!");
+		return -1;
+	}
+	fputc('A', fp);
+	fputc('B', fp);
+	fputc('C', fp);
+	fclose(fp);
+	return 0;
+}
