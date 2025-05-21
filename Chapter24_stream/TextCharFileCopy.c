304fcc71972f1145912a865d4853acab751338fd
kiwan8048 <koh0916b@gmail.com>
2025-05-21 19:56:27 +0900

    Add files via upload

---
+++ Chapter24_stream/TextCharFileCopy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE* wsrc = fopen("src.txt", "wt"); // 쓰기 모드로 파일 생성
+	int ch;
+	fputc('A', wsrc);
+	fputs("MY name is Kwon! \n", wsrc);
+	fclose(wsrc);
+	FILE* src = fopen("src.txt", "rt");
+	FILE* des = fopen("des.txt", "wt");
+	if (src == NULL || des == NULL)
+	{
+		printf("파일오픈 실패!");
+		return -1;
+	}
+	while ((ch = fgetc(src)) != EOF)
+	{
+		fputc(ch, des);
+	}
+	if (feof(src) != 0)
+	{
+		puts("파일복사 완료!");
+	}
+	else
+		puts("파일복사 실패!");
+	fclose(src);
+	fclose(des);
+	return 0;
+}
