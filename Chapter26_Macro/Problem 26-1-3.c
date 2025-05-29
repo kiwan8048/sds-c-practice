e63a33e857c86b7726bc230c985f46b5e4fa9ae5
kiwan8048 <koh0916b@gmail.com>
2025-05-29 18:40:06 +0900

    Add files via upload

---
+++ Chapter26_Macro/Problem 26-1-3.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX(A, B) ((A) >= (B) ? (A) : (B))
+
+int main(void)
+{
+	double A, B;
+	printf("두 개의 수 입력: ");
+	scanf("%lf %lf", &A, &B);
+	printf("%g가 더 큰 수 입니다 \n", MAX(A, B));
+	return 0;
+}
