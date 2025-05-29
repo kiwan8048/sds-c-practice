ba9384115a99846921abd4a8272e6c7bfec47b11
kiwan8048 <koh0916b@gmail.com>
2025-05-29 18:03:24 +0900

    Add files via upload

---
+++ Chapter26_Macro/if_endif.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ADD 1
+#define MIN 0
+
+int main(void)
+{
+	int num1, num2;
+	printf("두 개의 정수 입력: ");
+	scanf("%d %d", &num1, &num2);
+#if ADD // Add가 참이면
+	printf("%d + %d = %d \n", num1, num2, num1 + num2);
+#endif
+#if MIN // MIN이 참이면
+	printf("%d - %d = %d \n", num1, num2, num1 - num2);
+#endif
+	return 0;
+}
