1da2f52eaebbaccbf526d943add1461653ebb114
kiwan8048 <koh0916b@gmail.com>
2025-05-29 18:04:23 +0900

    Update MacroFunctionWeekness.c

--- Chapter26_Macro/MacroFunctionWeekness.c
+++ Chapter26_Macro/MacroFunctionWeekness.c
@@ -1,10 +1,11 @@
+// 이 코드는 교재의 내용을 참고하였습니다
 #include <stdio.h>
 
-#define DiffABS(X, Y) (((x)>(y))?((x)-(y)):((y)-(x))) // ��ҹ��� �Ǽ�
+#define DiffABS(X, Y) (((x)>(y))?((x)-(y)):((y)-(x))) // 대소문자 실수
 
 int main(void)
 {
-	printf("�� ���� ��: %d \n", DiffABS(5, 7));
-	printf("�� ���� ��: %g \n", DiffABS(1.8, -1.4));
+	printf("두 값의 차: %d \n", DiffABS(5, 7));
+	printf("두 값의 차: %g \n", DiffABS(1.8, -1.4));
 	return 0;
 }
