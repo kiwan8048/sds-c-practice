57ebdad41c5ac7e72d24370b64166ae796673546
kiwan8048 <koh0916b@gmail.com>
2025-05-29 18:04:43 +0900

    Update elif_endif.c

--- Chapter26_Macro/elif_endif.c
+++ Chapter26_Macro/elif_endif.c
@@ -1,16 +1,17 @@
+// 이 코드는 교재의 내용을 참고하였습니다
 #include <stdio.h>
 #define HIT_NUM 7
 
 int main(void)
 {
 #if HIT_NUM==5
-	puts("��ũ�� ��� HIT_NUM�� ���� 5�Դϴ�");
-#elif HIT_NUM==6 // elif ���
-	puts("��ũ�� ��� HIT_NUM�� ���� 6�Դϴ�");
-#elif HIT_NUM==7 // elif ���
-	puts("��ũ�� ��� HIT_NUM�� ���� 7�Դϴ�");
+	puts("매크로 상수 HIT_NUM은 현재 5입니다");
+#elif HIT_NUM==6 // elif 사용
+	puts("매크로 상수 HIT_NUM은 현재 6입니다");
+#elif HIT_NUM==7 // elif 사용
+	puts("매크로 상수 HIT_NUM은 현재 7입니다");
 #else 
-	puts("��ũ�� ��� HIT_NUM�� 5, 6, 7�� Ȯ���� �ƴմϴ�.");
+	puts("매크로 상수 HIT_NUM은 5, 6, 7은 확실히 아닙니다.");
 #endif
 	return 0;
 }
